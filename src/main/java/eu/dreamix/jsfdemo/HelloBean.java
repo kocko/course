cc42c21720753d3cf4c5374e948176e7fb3812ff
kocko <konstantin.yovkov@gmail.com>
2014-03-22 20:23:56 +0200

    Run the index page successfully

---
+++ src/main/java/eu/dreamix/jsfdemo/HelloBean.java
@@ -0,0 +1,11 @@
+package eu.dreamix.jsfdemo;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="helloBean")
+public class HelloBean {
+	
+	public String getGreeting() {
+		return "Hello";
+	}
+}
