fd76b4f377eb8d2ceaa252752593080916beac7a
kocko <konstantin.yovkov@gmail.com>
2014-05-26 09:28:19 +0300

    Introduced Spring and MongoDB to the system. Modified header width and updated to Primefaces 5.0

---
+++ src/main/java/bg/learnit/course/service/UsersService.java
@@ -0,0 +1,16 @@
+package bg.learnit.course.service;
+
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+public class UsersService {
+
+//	@Autowired
+//	private UsersDAO usersDao;
+//	
+//	public List<Users> findAll() {
+//		return usersDao.findAll();
+//	}
+}
